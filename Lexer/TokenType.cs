da6c96588abef91a6de886b3eab4bb6ce9bd5f9a
Amier Alchazli <amier1234@hotmail.com>
2023-03-20 15:24:20 +0100

    Auto stash before merge of "master" and "origin/master"

--- Lexer/TokenType.cs
+++ Lexer/TokenType.cs
@@ -2,9 +2,19 @@
 
 namespace eigen_parser;
 
+<<<<<<< Updated upstream:Lexer/TokenType.cs
 public enum TokenType
 {
     String,
     
     
+=======
+public enum Regex{
+    String,
+    Number,
+    
+
+
+
+>>>>>>> Stashed changes:Lexer/Regex.cs
 }
